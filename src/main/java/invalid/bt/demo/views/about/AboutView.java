c91345e84b5be57f74558bd61b863be419663a75
Benjamin <benjamin.guy.thomas@gmail.com>
2021-01-01 09:11:49 +0100

    Start

---
+++ src/main/java/invalid/bt/demo/views/about/AboutView.java
@@ -0,0 +1,18 @@
+package invalid.bt.demo.views.about;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import invalid.bt.demo.views.main.MainView;
+
+@Route(value = "about", layout = MainView.class)
+@PageTitle("About")
+public class AboutView extends Div {
+
+    public AboutView() {
+        setId("about-view");
+        add(new Text("Content placeholder"));
+    }
+
+}
